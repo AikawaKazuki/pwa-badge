6a5759906c98af4418a41c05a3e64bcd396d6e3a
AIKAWA <aikawa-kazuki@peace-and-passion.com>
2024-10-24 14:25:58 +0900

    webkit.org badging-for-home-screen-web-apps

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,23 @@
-self.addEventListener('push', function(event) {
-    const data = event.data ? event.data.json() : { title: 'Default Title', body: 'Default message', count: 1 };
+// https://webkit.org/blog/14112/badging-for-home-screen-web-apps/
+// Function to determine the badge count based on the event data
+function determineBadgeCount(data) {
+    // Process the data to compute the badge count
+    return Math.floor(Math.random() * 9) + 1;
+}
 
-    const options = {
-        body: data.body,
-        icon: '/icon-192x192.png'
-    };
+self.addEventListener('push', (event) => {
+    let promises = [];
 
-    event.waitUntil(
-        self.registration.showNotification(data.title, options)
-    );
-
-    if (navigator.setAppBadge) {
-        navigator.setAppBadge(data.count).catch((error) => {
-            console.error('Failed to set app badge from service worker:', error);
-        });
-    } else {
-        console.warn('setAppBadge is not supported in the service worker');
+    if ('setAppBadge' in self.navigator) {
+        const badgeCount = determineBadgeCount(event.data);
+        // Promise to set the badge
+        const promise = self.navigator.setAppBadge(badgeCount);
+        promises.push(promise);
     }
+
+    // Promise to show a notification
+    promises.push(self.registration.showNotification("You've got mail!"));
+
+    // Finally...
+    event.waitUntil(Promise.all(promises));
 });
